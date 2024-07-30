b98c779a0085e67c53d74de3eced75f02b059f95
adrian.maciuc <adrian.maciuc@gmail.com>
2024-07-30 09:36:53 +0300

    add button

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -6,14 +6,15 @@ export default function Card(cards: CardInterface) {
       <li className="cs-item">
         <span className="cs-number">{cards.id}</span>
         <h3 className="cs-h3">{cards.teamName}</h3>
-
-        <p className="cs-item-text">Developer: {cards.developer}</p>
-        <p className="cs-item-text">QA: {cards.QA}</p>
-        <p className="cs-item-text">Manager: {cards.manager}</p>
-        <p className="cs-item-text">Task: {cards.task}</p>
-        <button className="btn btn-primary" type="button" id="view-button">
+        <div>
+          <p className="cs-item-text">Developer: {cards.developer}</p>
+          <p className="cs-item-text">QA: {cards.QA}</p>
+          <p className="cs-item-text">Manager: {cards.manager}</p>
+          <p className="cs-item-text">Task: {cards.task}</p>
+        </div>
+        <a href="" className="cs-button-6" aria-label="view task">
           View Task
-        </button>
+        </a>
       </li>
     </>
   );
